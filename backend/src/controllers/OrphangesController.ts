3a3e02c6ddd739af3e118470ab46ed18dfbe5f68
defauth98 <neto.daniribeiro@gmail.com>
2020-10-13 14:19:54 -0300

    Validacao de erros

--- backend/src/controllers/OrphangesController.ts
+++ backend/src/controllers/OrphangesController.ts
@@ -1,6 +1,7 @@
 import { Response, Request } from 'express';
 import { getRepository } from 'typeorm';
 import orphanageView from '../views/orphanages_view';
+import * as Yup from 'yup';
 
 import Orphanages from '../models/Orphanages';
 
@@ -47,8 +48,7 @@ export default {
       return { path: image.filename };
     });
 
-    // Cria uma instancia de orfanato
-    const orphanage = orphanagesRepository.create({
+    const data = {
       name,
       latitude,
       longitude,
@@ -57,7 +57,29 @@ export default {
       opening_hours,
       open_on_weekends,
       images,
+    };
+
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      latitude: Yup.number().required(),
+      longitude: Yup.number().required(),
+      about: Yup.string().required().max(300),
+      instructions: Yup.string().required(),
+      opening_hours: Yup.string().required(),
+      open_on_weekends: Yup.boolean().required(),
+      images: Yup.array(
+        Yup.object().shape({
+          path: Yup.string().required(),
+        })
+      ).required(),
     });
+
+    await schema.validate(data, {
+      abortEarly: false,
+    });
+
+    // Cria uma instancia de orfanato
+    const orphanage = orphanagesRepository.create(data);
 
     // Salva no banco de dados
     await orphanagesRepository.save(orphanage);
