96f588fdb38f5cfa3d62cfd0842e34060e8a4bcc
defauth98 <neto.daniribeiro@gmail.com>
2020-10-13 14:00:46 -0300

    Criando as views de orphanges e images

--- backend/src/controllers/OrphangesController.ts
+++ backend/src/controllers/OrphangesController.ts
@@ -1,15 +1,18 @@
 import { Response, Request } from 'express';
-
 import { getRepository } from 'typeorm';
+import orphanageView from '../views/orphanages_view';
+
 import Orphanages from '../models/Orphanages';
 
 export default {
   async index(request: Request, response: Response) {
     const orphanagesRepository = getRepository(Orphanages);
 
-    const orphanages = await orphanagesRepository.find();
+    const orphanages = await orphanagesRepository.find({
+      relations: ['images'],
+    });
 
-    return response.json(orphanages);
+    return response.json(orphanageView.renderMany(orphanages));
   },
 
   async show(request: Request, response: Response) {
@@ -17,9 +20,11 @@ export default {
 
     const orphanagesRepository = getRepository(Orphanages);
 
-    const orphanage = await orphanagesRepository.findOneOrFail(id);
+    const orphanage = await orphanagesRepository.findOneOrFail(id, {
+      relations: ['images'],
+    });
 
-    return response.json(orphanage);
+    return response.json(orphanageView.render(orphanage));
   },
 
   async create(request: Request, response: Response) {
